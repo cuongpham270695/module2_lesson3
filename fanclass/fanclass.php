d93937ab32e4999782f87d809ea0d9102ea87c01
cuongpham270695 <79822838+cuongpham270695@users.noreply.github.com>
2021-04-06 21:42:26 +0700

    Add files via upload

--- fanclass/fanclass.php
+++ fanclass/fanclass.php
@@ -1,47 +1,69 @@
 <?php
-class Fan{
-   private int $speed;
-   private bool $on;
-   private float $radius;
-   private string $color;
-   public function __construct(){
-       $this->slow = 1;
-       $this->medium=2;
-       $this->fast=3;
-       $this->speed=$this->slow;
-       $this->on=false;
-       $this->radius=5;
-       $this->color="blue";
-   }
-   public function setSpeed(int $speed):void{
-       $this->speed = $speed;
-   }
-   public function getSpeed():int{
-       return $this->speed;
-   }
-   public function setOn(bool $on):void{
-       $this->on = $on;
-   }
-   public function getOn():bool{
-       return $this->on;
-   }
-   public function setRadius(float $radius):void{
-       $this->radius=$radius;
-   }
-   public function getRadius():float{
-       return $this->radius;
-   }
-   public function setColor(string $color):void{
-       $this->color =$color;
-   }
-   public function getColor():string{
-       return $this->color;
-   }
-   public function toString(){
-       if($this->on){
-           echo "Fan is on: Speed = " . $this->getSpeed(). ". Color = ".$this->getColor().". Radius = ".$this->getRadius();
-       }else{
-           echo "Fan is off. Color = ".$this->getColor().". Radius = ".$this->getRadius();
-       }
-   }
+
+class Fan
+{
+    private int $speed;
+    private bool $on;
+    private float $radius;
+    private string $color;
+
+    public function __construct()
+    {
+        $this->slow = 1;
+        $this->medium = 2;
+        $this->fast = 3;
+        $this->speed = $this->slow;
+        $this->on = false;
+        $this->radius = 5;
+        $this->color = "blue";
+    }
+
+    public function setSpeed(int $speed): void
+    {
+        $this->speed = $speed;
+    }
+
+    public function getSpeed(): int
+    {
+        return $this->speed;
+    }
+
+    public function setOn(bool $on): void
+    {
+        $this->on = $on;
+    }
+
+    public function getOn(): bool
+    {
+        return $this->on;
+    }
+
+    public function setRadius(float $radius): void
+    {
+        $this->radius = $radius;
+    }
+
+    public function getRadius(): float
+    {
+        return $this->radius;
+    }
+
+    public function setColor(string $color): void
+    {
+        $this->color = $color;
+    }
+
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    public function toString()
+    {
+        if ($this->on) {
+            echo "Fan is on: Speed = " . $this->getSpeed() . ". Color = " . $this->getColor() . ". Radius = " . $this->getRadius();
+        } else {
+            echo "Fan is off. Color = " . $this->getColor() . ". Radius = " . $this->getRadius();
+        }
+    }
 }
